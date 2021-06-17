c6ce7e0d1f760854a2a81c945367586ece1e523b
Bokriachraf <76520665+Bokriachraf@users.noreply.github.com>
2021-06-18 00:53:43 +0200

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,28 @@
+INSERT INTO Product VALUES
+(
+'P01',
+'Samsung Galaxy S20',
+'Smartphone',
+3299
+);
+INSERT INTO Product VALUES
+(
+'P02',
+'ASUS Notebook',
+'PC',
+4599
+
+);
+
+INSERT INTO Customer VALUES
+(
+'CO1',
+'ALI',
+'71321009');
+
+INSERT INTO Customer VALUES
+(
+'CO2',
+'ASMA',
+'77345823'
+);
